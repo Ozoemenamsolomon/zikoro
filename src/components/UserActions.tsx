b97bb0b537d7d02bbbfa00524667938cfc831a11
solozo <ozoemenamsolomon@yahoo.com>
2023-12-12 19:08:04 +0100

    fix disappearing sidebar text

--- src/components/UserActions.tsx
+++ src/components/UserActions.tsx
@@ -1,30 +1,38 @@
-'use client';
-import React from 'react';
-import Image from 'next/image';
-import { LogOut } from 'styled-icons/ionicons-outline';
-import { PersonFeedback } from 'styled-icons/fluentui-system-filled';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { LogOut } from "styled-icons/ionicons-outline";
+import { PersonFeedback } from "styled-icons/fluentui-system-filled";
 
 export const UserActions = () => {
-	return (
-		<div className="flex flex-col gap-4 border-t p-4 border-basebody">
-			<div className="flex items-center gap-2">
-				<Image
-					src={'/b92cf7b1b06acc1b9a0759b6f97724c349488816.webp'}
-					alt={'user avatar'}
-					width={40}
-					height={40}
-					className="rounded-full"
-				/>
-				<p>User Name</p>
-			</div>
-			<button className="flex gap-2">
-				<PersonFeedback className="w-6 h-6" />
-				Give feedback
-			</button>
-			<button className="flex gap-2 text-red-700">
-				<LogOut className="w-6 h-6" />
-				Log out
-			</button>
-		</div>
-	);
+  const {
+    user: { name: userName, avatar },
+  } = {
+    user: {
+      name: "User Name",
+      avatar: "/b92cf7b1b06acc1b9a0759b6f97724c349488816.webp",
+    },
+  };
+  return (
+    <div className="flex flex-col gap-4 border-t p-4 border-basebody">
+      <div className="flex items-center gap-2">
+        <Image
+          src={avatar}
+          alt={"user avatar"}
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+        <p className="text-black">{userName}</p>
+      </div>
+      <button className="flex gap-2 text-black">
+        <PersonFeedback className="w-6 h-6" />
+        Give feedback
+      </button>
+      <button className="flex gap-2 text-red-700">
+        <LogOut className="w-6 h-6" />
+        Log out
+      </button>
+    </div>
+  );
 };
