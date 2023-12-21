c5b1b43de83b0d633ba69ed130a95f805314a9f3
Yusuf Bilal <ubahyusuf484@gmail.com>
2023-12-21 20:31:30 +0100

    refined overlay component

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { Close } from "styled-icons/evaicons-solid";
 
-const Overlay = ({ isOpen, onClose, children }) => {
+type TOverlayProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  title: string;
+};
+
+const Overlay = ({ isOpen, onClose, title, children }: TOverlayProps) => {
   return (
     <>
       {isOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-md max-w-md w-full">
-            <button
-              className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
-              onClick={onClose}
-            >
-              Close
-            </button>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 py-4 max-h-screen">
+          <div className="bg-white p-4 rounded-md max-w-lg w-full relative max-h-full overflow-auto no-scrollbar">
+            <div className="mb-4 flex justify-between items-center">
+              <h2 className="font-semibold text-slate-800 text-lg">{title}</h2>
+              <button
+                className="text-gray-700 hover:text-slate-900"
+                onClick={onClose}
+              >
+                <Close className="w-5 h-5" />
+              </button>
+            </div>
             {children}
           </div>
         </div>
