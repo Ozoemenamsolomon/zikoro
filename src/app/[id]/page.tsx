4ea9ca006cbe79cdfc0bf131852a52104a500b66
sekani50 <mohammedadeoye0@gmail.com>
2024-01-17 22:09:44 -0800

    feat: updated nav section

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,19 +1,30 @@
 "use client";
 
 import { SideBarLayout } from "@/components";
-import { EventCards } from "@/components/home";
+import { EventCards, EmptyCard } from "@/components/home";
 import { useFetchOrganizationEvents } from "@/hooks";
-export default function Page({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  const { data: eventData, refetch } = useFetchOrganizationEvents(id)
+import { LoaderAlt } from "@styled-icons/boxicons-regular/LoaderAlt";
+
+export default function Page({ params: { id } }: { params: { id: string } }) {
+  const { data: eventData, refetch, loading } = useFetchOrganizationEvents(id);
 
   return (
     <main className="w-full h-full">
       <SideBarLayout isHomePage={true}>
-        <EventCards refetch={refetch} events={eventData} />
+        {loading && (
+          <div className="w-full h-[300px] flex items-center justify-center">
+            <LoaderAlt size={50} className="animate-spin" />
+          </div>
+        )}
+
+        {!loading && eventData.length > 0 && (
+          <EventCards refetch={refetch} events={eventData} />
+        )}
+        {!loading && eventData?.length === 0 && (
+          <EmptyCard
+            text={`You have not added any event to this organization`}
+          />
+        )}
       </SideBarLayout>
     </main>
   );
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -5,58 +5,71 @@ import Link from "next/link";
 import { links } from "@/constants";
 import { useParams, usePathname } from "next/navigation";
 import { cn } from "@/lib";
-import { Button } from "@/components"
+import { Button } from "@/components";
 
-export const NavLinks = ({ query, id }: {id: string | string[], query: string | null }) => {
- 
+export const NavLinks = ({
+  query,
+  id,
+}: {
+  id: string | string[];
+  query: string | null;
+}) => {
   const pathname = usePathname();
 
-
   return (
-    <ul className="flex flex-col gap-y-1 items-start  justify-start w-full">
+    <ul className="flex flex-col gap-y-1 items-start h-[75%] pb-10  no-scrollbar overflow-y-auto  justify-start w-full">
       {links.map(({ href, name, icon: Icon }, idx) => {
-
         if (idx === links.length - 1) {
           return (
-            <li key={name} className={cn("h-fit w-full", query === null && "hidden")}>
+            <li
+              key={name}
+              className={cn("h-fit w-full", query === null && "hidden")}
+            >
               <Button
                 className={cn(
                   "p-3 px-4 flex  items-center justify-start gap-x-2  w-full",
                   href === pathname &&
-                  "text-zikoro  bg-zikoro border-l-4 border-zikoro bg-opacity-10  "
+                    "text-zikoro  bg-zikoro border-l-4 border-zikoro bg-opacity-10  "
                 )}
               >
-                {Icon && <Icon color={href === pathname ? "#001FCC" : "black"} />}
+                {Icon && (
+                  <Icon color={href === pathname ? "#001FCC" : "black"} />
+                )}
                 <span>{name}</span>
               </Button>
               <ul className="w-full flex flex-col gap-y-1 pl-12 text-xs sm:text-sm items-start justify-start">
                 <li className="w-full">
-                  <Link href={`${href}/${id}?organization=${query}`}>{query}</Link>
+                  <Link href={`${href}/${id}?organization=${query}`}>
+                    {query}
+                  </Link>
                 </li>
-
               </ul>
             </li>
-          )
-        }
-        else {
+          );
+        } else {
           return (
             <li key={name} className="w-full">
               <Link
-                href={href === "/events" ? `${href}/organization/${id}?organization=${query}` : href}
+                href={
+                  href === "/events"
+                    ? `${href}/organization/${id}?organization=${query}`
+                    : href
+                }
                 className={cn(
                   "p-3 px-4 flex  items-center gap-x-2  w-full",
-                  href === pathname || pathname.includes("events") &&
-                  "text-zikoro  bg-zikoro border-l-4 border-zikoro bg-opacity-10  ",
+                  href === pathname &&
+                    "text-zikoro  bg-zikoro border-l-4 border-zikoro bg-opacity-10  ",
                   query === null && href === "/events" && "hidden"
                 )}
               >
-                {Icon && <Icon color={href === pathname ? "#001FCC" : "black"} />}
+                {Icon && (
+                  <Icon color={href === pathname ? "#001FCC" : "black"} />
+                )}
                 <span>{name}</span>
               </Link>
             </li>
           );
         }
-
       })}
     </ul>
   );
