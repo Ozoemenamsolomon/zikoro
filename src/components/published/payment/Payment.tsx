76be79986bf38d201fb8b7858cd543c8d510ba7b
sekani50 <mohammedadeoye0@gmail.com>
2024-01-20 15:19:51 -0800

    fix: dummy pricecategory

--- src/components/published/payment/Payment.tsx
+++ src/components/published/payment/Payment.tsx
@@ -64,7 +64,7 @@ export function Payment({
       currency: "NGN",
       paidStatus: reference.status === "success",
       eventDate,
-      priceCategory,
+      priceCategory: priceCategory || "Standard",
       event: eventTitle,
       attendeesDetails,
       eventPrice,
