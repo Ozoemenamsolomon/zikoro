1d8b1f8ab42086e0c6df7ff117442944592c67e8
Yusuf Bilal <ubahyusuf484@gmail.com>
2024-01-26 14:52:27 +0100

    feat: added isRequired props

--- src/components/InputOffsetLabel.tsx
+++ src/components/InputOffsetLabel.tsx
@@ -2,21 +2,24 @@ import {
   FormControl,
   FormItem,
   FormLabel,
-  FormMessage
+  FormMessage,
 } from "@/components/ui/form";
 import React from "react";
 
 export default function InputOffsetLabel({
   children,
   label,
+  isRequired,
 }: {
   children: React.ReactNode;
   label: string;
+  isRequired?: boolean;
 }) {
   return (
     <FormItem className="relative">
       <FormLabel className="absolute top-0 -translate-y-1/2 right-4 bg-white text-gray-600 text-tiny px-1">
         {label}
+        {isRequired && <sup className="text-red-700">*</sup>}
       </FormLabel>
       <FormControl>{children}</FormControl>
       <FormMessage />
