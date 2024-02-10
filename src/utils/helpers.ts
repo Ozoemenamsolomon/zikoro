69141a9a75be16ad0a760a3ac0e362b69b760c41
Yusuf Bilal <ubahyusuf484@gmail.com>
2024-02-10 12:17:01 +0100

    feat: added cloudinary upload function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -114,3 +114,30 @@ export function findKeysWithSharedValue(
     )
   );
 }
+
+export function uploadFiles(files: File[]) {
+  const url =
+    "https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME}/image/upload";
+
+  const formData = new FormData();
+
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    formData.append("file", file);
+    // formData.append("upload_preset", "docs_upload_example_us_preset");
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        alert("files uploaded successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
