55ddc7c54b688a548b7b2283fe26270af610b36a
sekani50 <mohammedadeoye0@gmail.com>
2024-03-04 15:07:49 -0800

    updated

--- src/components/content/TextEditor.tsx
+++ src/components/content/TextEditor.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
+import { Quill } from "react-quill";
 const QuillEditor = dynamic(() => import("react-quill"), { ssr: false });
 
 export default function TextEditor({
@@ -11,6 +12,26 @@ export default function TextEditor({
   defaultValue: string | undefined
 }) {
 
+  const CustomUndo = () => (
+    <svg viewBox="0 0 18 18">
+      <polygon className="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10" />
+      <path
+        className="ql-stroke"
+        d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"
+      />
+    </svg>
+  );
+  
+  // Redo button icon component for Quill editor
+  const CustomRedo = () => (
+    <svg viewBox="0 0 18 18">
+      <polygon className="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10" />
+      <path
+        className="ql-stroke"
+        d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"
+      />
+    </svg>
+  );
  
   const quillModules = {
     toolbar: {
@@ -31,8 +52,8 @@ export default function TextEditor({
       //  [{ align: [] }],
         ["link"], // "image", "video"
         ["clean"],
-        ["undo"],
-        ["redo"],
+        ["undo", "redo"],
+
        // ["imageResize", "imageTextAlternative"],
       ],
     },
