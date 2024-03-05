cc84f5028be0c1c74bef04b83dd6dd52ac2edd44
Langrangee <langrangee@000.local>
2024-03-05 13:38:08 +0100

    My first commit, all error fixed

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,17 +1,33 @@
-import React from "react"
-import Navbar from "../../components/index/Navbar"
-import Section1 from "../../components/index/Section1"
-import Section2 from "../../components/index/Section2"
-import Section3 from "../../components/index/Section3"
-const Home = () => {
+'use client'
+import Navbar from "@/components/index/Navbar"
+import Section1 from "@/components/index/Section1"
+import Section2 from "@/components/index/Section2"
+import Section3 from "@/components/index/Section3"
+import Features from "@/components/index/Features"
+import Section4 from "@/components/index/Section4"
+import Section5 from "@/components/index/Section5"
+import Footer from "@/components/index/Footer"
+import { Montserrat } from "next/font/google";
 
-  return <div className="">
-            <Navbar/>
-            <Section1/>
-            <Section2/>
-            {/* <Section3/> */}
-        </div>
-      ;
-};
+const montserrat = Montserrat({
+  weight: ['100', '200','300', '400', '500','600', '700'],
+  subsets: ['latin'],
+  display:'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
 
-export default Home;
+
+export default function Home() {
+  return (
+    <div className={`${montserrat.className} `}>
+        <Navbar/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Features/>
+        <Section4/>
+        <Section5/>
+        <Footer/>
+    </div>
+  )
+}
