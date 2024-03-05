27229c852953408007b200ef1818b7bb2c6d8f2a
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-03-05 08:57:06 +0100

    fix: next export error

--- src/app/(mainApp)/people/layout.tsx
+++ src/app/(mainApp)/people/layout.tsx
@@ -5,15 +5,8 @@ import { Button } from "@/components/ui/button";
 import { calculateAndSetMaxHeight } from "@/utils/helpers";
 import { useRef, useState, useLayoutEffect, useEffect } from "react";
 import { TLink } from "@/components/Sidebar";
+import { PeopleLinks } from "./links";
 
-export const PeopleLinks: TLink[] = [
-  { name: "All", href: "all" },
-  { name: "released certificates", href: "released_certificates" },
-  { name: "favorites", href: "favorites" },
-  { name: "tags", href: "tags" },
-  { name: "notes", href: "notes" },
-  { name: "invites", href: "invites" },
-];
 
 const People = ({ children }: { children: React.ReactNode }) => {
   const pathNames = usePathname().split("/");
