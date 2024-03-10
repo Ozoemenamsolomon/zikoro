9df60c7339dbc04e77e71b0be0eb0898232f819e
sekani50 <mohammedadeoye0@gmail.com>
2024-03-09 22:55:12 -0800

    uuu

--- src/components/marketPlace/rewards/Rewards.tsx
+++ src/components/marketPlace/rewards/Rewards.tsx
@@ -6,6 +6,8 @@ import { Button } from "@/components";
 import { useState } from "react";
 import { useFetchSingleEvent, useFetchRewards } from "@/hooks";
 import { CreateReward, RewardCard } from "./_components";
+import { EmptyCard } from "@/components/composables";
+import { Loader2 } from "styled-icons/remix-fill";
 import { Reward } from "@/types";
 export default function Rewards({ eventId }: { eventId: string }) {
   const [isOpen, setOpen] = useState(false);
@@ -34,6 +36,15 @@ export default function Rewards({ eventId }: { eventId: string }) {
       </div>
 
       <div className="w-full mt-3 sm:mt-5  grid gap-4 px-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {loading && (
+          <div className="w-full col-span-full h-[60vh] flex items-center justify-center">
+            <Loader2 size={50} className="animate-spin" />
+          </div>
+        )}
+        {!loading && Array.isArray(data) && data?.length === 0 && (
+          <EmptyCard height="80" width="82" text="No available Reward" />
+        )}
+
         {Array.isArray(data) &&
           data?.map((item, index) => <RewardCard key={index} reward={item} />)}
       </div>
