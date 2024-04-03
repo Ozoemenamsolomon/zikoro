16d680ffb39b7c4a34f2228d487ceabd3cdfe581
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-04-03 17:01:41 +0100

    fix

--- src/components/MobileBottomNav.tsx
+++ src/components/MobileBottomNav.tsx
@@ -6,7 +6,7 @@ import {
   HomeIcon,
   NotificationIcon,
   UserIcon,
-} from "@/components/constants";
+} from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Dot } from "@styled-icons/bootstrap/Dot";
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export * from "./icons"
-export * from "./constants"
+export * from "."
 export * from "./data"
