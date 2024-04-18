bfd6a8627ea82f53e99bfc2ddf8f3ca36352e1c7
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-04-18 16:56:14 +0100

    feat: WIP, payout page

--- src/app/api/billing/route.ts
+++ src/app/api/billing/route.ts
@@ -2,19 +2,31 @@ import { NextRequest, NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { userId: string } }
-) {
+export async function GET(req: NextRequest) {
   const supabase = createRouteHandlerClient({ cookies });
   if (req.method === "GET") {
     try {
-      const { userId } = params;
+      const { searchParams } = new URL(req.url);
+      const userId = searchParams.get("userId");
+      const payOutStatus = searchParams.get("payOutStatus");
+      const registrationCompleted = searchParams.get("registrationCompleted");
 
-      const { data, error, status } = await supabase
+      console.log(registrationCompleted);
+
+      const query = supabase
         .from("eventTransactions")
-        .select("*, events!inner(*)")
-        .eq("userId", userId);
+        .select("*, events!inner(*)");
+
+      if (userId) query.eq("userId", userId);
+      if (registrationCompleted)
+        query.eq(
+          "registrationCompleted",
+          parseInt(registrationCompleted) === 1 ? true : false
+        );
+      if (payOutStatus && parseInt(payOutStatus) === 1)
+        query.neq("payOutStatus", "new");
+
+      const { data, error, status } = await query;
 
       if (error) throw error;
 
