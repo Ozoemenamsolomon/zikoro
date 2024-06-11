1d68cd37b5b7a4e0136b5f161ba8697ce187f4ae
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-06-11 22:44:18 +0100

    push

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -75,10 +75,42 @@ export async function GET(req: NextRequest) {
 
       if (error) throw error;
 
+      console.log(data);
+
       return NextResponse.json(
         { data: DUMMY_DATA },
         {
           status: 200,
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        {
+          error: "An error occurred while making the request.",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  } else {
+    return NextResponse.json({ error: "Method not allowed" });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const supabase = createRouteHandlerClient({ cookies });
+  if (req.method === "POST") {
+    try {
+      const payload = await req.json();
+
+      const { error } = await supabase.from("Leads").insert(payload);
+      if (error) throw error;
+      return NextResponse.json(
+        { msg: "favourites updated successfully" },
+        {
+          status: 201,
         }
       );
     } catch (error) {
