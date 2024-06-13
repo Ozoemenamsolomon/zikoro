e2b9d70d084956fcc779009e37aa226b82748e1d
emmaudeji-zbook <gogreneinfo@gmail.com>
2024-06-13 11:01:43 +0100

    setting up appointment scheduling features

---
+++ src/app/appointments/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Schedule and manage appointments`,
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
