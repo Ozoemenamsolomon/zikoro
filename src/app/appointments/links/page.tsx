f24f39103a581458ec9ef90c87115549b9ffcf92
sekani50 <mohammedadeoye0@gmail.com>
2024-06-24 10:36:48 -0800

    fix: bb

--- src/app/appointments/links/page.tsx
+++ src/app/appointments/links/page.tsx
@@ -1,14 +1,15 @@
 import Main from '@/components/appointments/Main'
 import LinksPage from '@/components/appointments/links'
-import { supabaseServerClient } from '@/utils/supabase/server'
 import React from 'react'
 
 const page = async ({ searchParams }: { searchParams: { [key: string]: string | string[] } }) => {
-  const {data,error} = await supabaseServerClient.from('appointmentLinks').select('*')
-  console.log({data,error, searchParams}, )
+ /**
+   const {data,error} = await supabaseServerClient.from('appointmentLinks').select('*')
+  */
+ 
   return (
     <Main>
-        <LinksPage appointmnetLinks={data || []}/>
+        <LinksPage appointmnetLinks={[]}/>
     </Main>
   )
 }
