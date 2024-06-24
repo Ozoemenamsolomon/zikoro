bd416dfe22041fb7b46d9203bf6b2ab39acbd822
sekani50 <mohammedadeoye0@gmail.com>
2024-06-24 09:45:17 -0800

    fix: build error

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Appointments from '@/components/appointments/Appointments'
 import Main from '@/components/appointments/Main'
 import { supabaseServerClient } from '@/utils/supabase/server'
@@ -5,7 +7,7 @@ import React from 'react'
 
 const AppointmentsPage = async ({children}:{children:React.ReactNode}) => {
   const {data,error} = await supabaseServerClient.from('appointmentLinks').select('*')
-  console.log({data,error})
+  // console.log({data,error})
 
   return (
     <Main>
