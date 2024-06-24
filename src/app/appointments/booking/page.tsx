5517357e6e40830e0621ab85a240ff929d7914be
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-06-24 18:19:20 +0100

    push

--- src/app/appointments/booking/page.tsx
+++ src/app/appointments/booking/page.tsx
@@ -1,20 +1,22 @@
-import Booking from '@/components/appointments/booking'
-import { supabaseServerClient } from '@/utils/supabase/server'
-import React from 'react'
+import Booking from "@/components/appointments/booking";
+import { supabaseServerClient } from "@/utils/supabase/server";
+import React from "react";
 
-const BookingPage = async ({ searchParams }: { searchParams: { [key: string]: string | string[] } }) => {
-  const {data,} = await supabaseServerClient
-                              .from('appointmentLinks')
-                              .select('*')
-                              .eq('id', searchParams?.b)
-                              .single()
+const BookingPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] };
+}) => {
+  const { data } = await supabaseServerClient
+    .from("appointmentLinks")
+    .select("*")
+    .eq("id", searchParams?.b)
+    .single();
 
-const {data:user,error} = await supabaseServerClient.auth.getUser()
-console.log({data, user, error})
+  const { data: user, error } = await supabaseServerClient.auth.getUser();
+  console.log({ data, user, error });
 
-  return (
-    <Booking appointmnetLink={data} />
-  )
-}
+  return <Booking appointmnetLink={data} />;
+};
 
-export default BookingPage
+export default BookingPage;
