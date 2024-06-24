6cca59414010fb6b819eb6905b77711b544b1708
sekani50 <mohammedadeoye0@gmail.com>
2024-06-24 12:17:04 -0800

    fix: issu

--- src/app/appointments/links/page.tsx
+++ src/app/appointments/links/page.tsx
@@ -2,7 +2,7 @@ import Main from '@/components/appointments/Main'
 import LinksPage from '@/components/appointments/links'
 import React from 'react'
 
-const page = async ({ searchParams }: { searchParams: { [key: string]: string | string[] } }) => {
+const page = async () => {
  /**
    const {data,error} = await supabaseServerClient.from('appointmentLinks').select('*')
   */
