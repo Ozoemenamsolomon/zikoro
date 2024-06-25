e81e3a382c5c2143c752b880098d3352fa2fca09
sekani50 <mohammedadeoye0@gmail.com>
2024-06-25 11:58:34 -0800

    fix: uncomment middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,8 +21,7 @@ export async function middleware(req: NextRequest) {
     data: { session },
   } = await supabase.auth.getSession();
 
-/**
- 
+
    // Check if the request path is included
   const isIncludedPath = includedPaths.some((path) =>
     req.nextUrl.pathname.startsWith(path)
@@ -45,7 +44,6 @@ export async function middleware(req: NextRequest) {
 
   // Allow the request to proceed if user is authenticated or the path is not included
   return res;
- */
  
  
 
