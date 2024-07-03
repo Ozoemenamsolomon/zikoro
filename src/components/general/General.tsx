4102246f87274843b97b9d91b07515fc32c8ffdb
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2024-07-03 20:22:32 +0100

    update:added the contact field and social links to workspace, updated the delete modal

--- src/components/general/General.tsx
+++ src/components/general/General.tsx
@@ -1,5 +1,14 @@
 "use client";
-import { GeneralImageIcon, GeometryIcon, TrashIcon } from "@/constants";
+import {
+  GeneralImageIcon,
+  GeometryIcon,
+  SocialLinksIcon,
+  TrashIcon,
+  DashboardXIcon,
+  DashboardLinkendinIcon,
+  DashboardInstagramIcon,
+  DashboardFacebookIcon,
+} from "@/constants";
 import React, { useEffect, useState } from "react";
 import useOrganizationStore from "@/store/globalOrganizationStore";
 import {
@@ -16,10 +25,262 @@ import {
 export default function General() {
   const { organization, setOrganization } = useOrganizationStore();
   const [currentUser, setCurrentUser] = useState<any>(null);
+  const [delInput, setDelInput] = useState<string>("");
 
   const updateSetting = (e: any) => {
     e.prevntDefault();
   };
+  const countryList = [
+    "Afghanistan",
+    "Åland Islands",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas (the)",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia (Plurinational State of)",
+    "Bonaire, Sint Eustatius and Saba",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Bouvet Island",
+    "Brazil",
+    "British Indian Ocean Territory (the)",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cabo Verde",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cayman Islands (the)",
+    "Central African Republic (the)",
+    "Chad",
+    "Chile",
+    "China",
+    "Christmas Island",
+    "Cocos (Keeling) Islands (the)",
+    "Colombia",
+    "Comoros (the)",
+    "Congo (the Democratic Republic of the)",
+    "Congo (the)",
+    "Cook Islands (the)",
+    "Costa Rica",
+    "Croatia",
+    "Cuba",
+    "Curaçao",
+    "Cyprus",
+    "Czechia",
+    "Côte d'Ivoire",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic (the)",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia",
+    "Falkland Islands (the) [Malvinas]",
+    "Faroe Islands (the)",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Guiana",
+    "French Polynesia",
+    "French Southern Territories (the)",
+    "Gabon",
+    "Gambia (the)",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guadeloupe",
+    "Guam",
+    "Guatemala",
+    "Guernsey",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Heard Island and McDonald Islands",
+    "Holy See (the)",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran (Islamic Republic of)",
+    "Iraq",
+    "Ireland",
+    "Isle of Man",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Korea (the Democratic People's Republic of)",
+    "Korea (the Republic of)",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Lao People's Democratic Republic (the)",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macao",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands (the)",
+    "Martinique",
+    "Mauritania",
+    "Mauritius",
+    "Mayotte",
+    "Mexico",
+    "Micronesia (Federated States of)",
+    "Moldova (the Republic of)",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands (the)",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger (the)",
+    "Nigeria",
+    "Niue",
+    "Norfolk Island",
+    "Northern Mariana Islands (the)",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Palestine, State of",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines (the)",
+    "Pitcairn",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Republic of North Macedonia",
+    "Romania",
+    "Russian Federation (the)",
+    "Rwanda",
+    "Réunion",
+    "Saint Barthélemy",
+    "Saint Helena, Ascension and Tristan da Cunha",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Martin (French part)",
+    "Saint Pierre and Miquelon",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Sint Maarten (Dutch part)",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Georgia and the South Sandwich Islands",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan (the)",
+    "Suriname",
+    "Svalbard and Jan Mayen",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic",
+    "Taiwan (Province of China)",
+    "Tajikistan",
+    "Tanzania, United Republic of",
+    "Thailand",
+    "Timor-Leste",
+    "Togo",
+    "Tokelau",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks and Caicos Islands (the)",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates (the)",
+    "United Kingdom of Great Britain and Northern Ireland (the)",
+    "United States Minor Outlying Islands (the)",
+    "United States of America (the)",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Venezuela (Bolivarian Republic of)",
+    "Viet Nam",
+    "Virgin Islands (British)",
+    "Virgin Islands (U.S.)",
+    "Wallis and Futuna",
+    "Western Sahara",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+  ];
 
   useEffect(() => {
     if (organization) {
@@ -39,18 +300,17 @@ export default function General() {
               <p className="text-xl font-semibold">Basic Settings</p>
             </div>
             <button className="py-2 px-4 text-white text-[14px] bg-gradient-to-tr from-custom-gradient-start to-custom-gradient-end rounded-md ">
-              Save Setting
+              Save
             </button>
           </div>
 
           <div className="mt-8">
-            <p className="text-[14px] text-[#1f1f1f]">Workspace Name</p>
+            <label className="text-[14px] text-[#1f1f1f]">Workspace Name</label>
             <div className="w-full h-[45px] mt-2 ">
               <input
-                required
                 type="text"
                 value={currentUser?.organizationName}
-                name=""
+                name="orgName"
                 className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f]"
               />
             </div>
@@ -62,13 +322,19 @@ export default function General() {
 
           <div className="flex flex-col lg:flex-row gap-[22px] mt-8 ">
             <div className=" w-full lg:w-1/2">
-              <p className="text-[14px] text-[#1f1f1f]">Organization Type</p>
+              <label className="text-[14px] text-[#1f1f1f]">
+                Organization Type
+              </label>
               <div className="w-full h-[45px] mt-2 text-[15px] text-[#1f1f1f]">
                 <select
-                  name=""
+                  name="orgType"
                   value=""
                   className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end text-[14px] text-[#1f1f1f] outline-none px-[10px] py-[11px]"
                 >
+                  <option value="" className="">
+                    Private
+                  </option>
+
                   <option value="" className="">
                     Private
                   </option>
@@ -80,7 +346,7 @@ export default function General() {
               <p className="text-[14px] text-[#1f1f1f]">Pricing Plan</p>
               <div className="w-full h-[45px] mt-2 text-[15px] text-[#1f1f1f]">
                 <select
-                  name=""
+                  name="orgPlan"
                   value=""
                   className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end text-[14px] text-[#1f1f1f] outline-none px-[10px] py-[11px]"
                 >
@@ -103,35 +369,60 @@ export default function General() {
 
           <div className="flex flex-col lg:flex-row gap-[22px] mt-8 ">
             <div className=" w-full lg:w-1/2">
-              <p className="text-[14px] text-[#1f1f1f]">Organization Type</p>
+              <p className="text-[14px] text-[#1f1f1f]">Country</p>
               <div className="w-full h-[45px] mt-2 text-[15px] text-[#1f1f1f]">
                 <select
                   name=""
-                  value=""
+                  value="orgCountry"
                   className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end text-[14px] text-[#1f1f1f] outline-none px-[10px] py-[11px]"
                 >
-                  <option value="" className="">
-                    Private
-                  </option>
-                  <option value="" className="">
-                    Public
-                  </option>
+                  {countryList.map((country) => (
+                    <option
+                      value={country}
+                      className="bg-transparent text-black"
+                    >
+                      {country}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
 
             <div className=" w-full lg:w-1/2">
-              <p className="text-[14px] text-[#1f1f1f]">Country</p>
+              <p className="text-[14px] text-[#1f1f1f]">Phone Number</p>
               <div className="w-full h-[45px] mt-2 text-[15px] text-[#1f1f1f]">
-                <select
-                  name=""
+                <input
+                  type="tel"
                   value=""
-                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end text-[14px] text-[#1f1f1f] outline-none px-[10px] py-[11px]"
-                >
-                  <option value="" className="">
-                    Free
-                  </option>
-                </select>
+                  name="orgTel"
+                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f]"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col lg:flex-row gap-[22px] mt-8 ">
+            <div className=" w-full lg:w-1/2">
+              <p className="text-[14px] text-[#1f1f1f]">Whatsapp Number</p>
+              <div className="w-full h-[45px] mt-2 text-[15px] text-[#1f1f1f]">
+                <input
+                  type="tel"
+                  value=""
+                  name="orgWhatsappNumber"
+                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f]"
+                />
+              </div>
+            </div>
+
+            <div className=" w-full lg:w-1/2">
+              <p className="text-[14px] text-[#1f1f1f]">Email</p>
+              <div className="w-full h-[45px] mt-2 text-[15px] text-[#1f1f1f]">
+                <input
+                  type="email"
+                  value=""
+                  name="orgEmail"
+                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f]"
+                />
               </div>
             </div>
           </div>
@@ -174,9 +465,93 @@ export default function General() {
           </div>
         </div>
 
+        {/* Social media link */}
+        <div className="mt-8 mb-8 ml-0 lg:ml-[12px] mr-0 lg:mr-[47px] pl-3 lg:pl-[24px] pr-3 lg:pr-[114px]">
+          <div className="flex items-center gap-x-3 pt-[32px]">
+            <SocialLinksIcon />
+            <p className="text-xl font-semibold">Social Media Links</p>
+          </div>
+
+          <div className="mt-8 flex items-center gap-x-2 w-full">
+            <div className="mt-8">
+              <DashboardXIcon />
+            </div>
+            <div className="flex-1">
+              <label className="text-[14px] text-[#1f1f1f]">Twitter</label>
+              <div className="w-full h-[45px] mt-2 ">
+                <input
+                  required
+                  type="text"
+                  value=""
+                  name="orgName"
+                  placeholder="Enter Link"
+                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f] placeholder-black"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8 flex items-center gap-x-2 w-full">
+            <div className="mt-8">
+              <DashboardLinkendinIcon />
+            </div>
+            <div className="flex-1">
+              <label className="text-[14px] text-[#1f1f1f]">LinkendIn</label>
+              <div className="w-full h-[45px] mt-2 ">
+                <input
+                  required
+                  type="text"
+                  value=""
+                  name="orgName"
+                  placeholder="Enter Link"
+                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f] placeholder-black"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8 flex items-center gap-x-2 w-full">
+            <div className="mt-8">
+              <DashboardFacebookIcon />
+            </div>
+            <div className="flex-1">
+              <label className="text-[14px] text-[#1f1f1f]">Facebook</label>
+              <div className="w-full h-[45px] mt-2 ">
+                <input
+                  required
+                  type="text"
+                  value=""
+                  name="orgName"
+                  placeholder="Enter Link"
+                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f] placeholder-black"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8 flex items-center gap-x-2 w-full">
+            <div className="mt-8">
+              <DashboardInstagramIcon />
+            </div>
+            <div className="flex-1">
+              <label className="text-[14px] text-[#1f1f1f]">Instagram</label>
+              <div className="w-full h-[45px] mt-2 ">
+                <input
+                  required
+                  type="text"
+                  value=""
+                  name="orgName"
+                  placeholder="Enter Link"
+                  className="w-full h-full rounded-xl border-[1px] border-indigo-600 bg-gradient-to-tr from-custom-bg-gradient-start to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f] placeholder-black"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* Delete Section */}
 
-        <div className="mt-8 ml-0 lg:ml-[12px] mr-0 lg:mr-[47px] pl-3 lg:pl-[24px] pr-3 lg:pr-0 py-8 mb-16 lg:mb-2 ">
+        <div className="mt-8 ml-0 lg:ml-[12px] mr-0 lg:mr-[47px] pl-3 lg:pl-[24px] pr-3 lg:pr-[114px] py-8 mb-16 lg:mb-2 ">
           <div className="flex gap-x-3">
             <TrashIcon size={20} />
             <p className="font-semibold ">Delete Workspace</p>
@@ -188,35 +563,43 @@ export default function General() {
           <div className="flex justify-center lg:justify-end mt-8 ">
             <Dialog>
               <DialogTrigger asChild>
-                <button className="py-2 px-4 text-white text-[15px] bg-gradient-to-tr from-custom-gradient-start to-custom-gradient-end rounded-md ">
+                <button className="py-2 px-4 text-white text-[15px] bg-[#E74C3C] font-medium rounded-md ">
                   Delete Workspace
                 </button>
               </DialogTrigger>
               <DialogContent className="sm:max-w-md">
                 <DialogHeader>
-                  <DialogTitle>Share link</DialogTitle>
+                  <DialogTitle className="boder-[1px] border-gray-200 pb-2">
+                    Delete Your Workspace
+                  </DialogTitle>
                   <DialogDescription>
-                    Anyone who has this link will be able to view this.
+                    This will permanently delete this workspace. Please enter
+                    <span className="font-semibold">
+                    {" "} {currentUser?.organizationName}{" "}
+                    </span>
+                    to confirm deletion
                   </DialogDescription>
                 </DialogHeader>
-                <div className="flex items-center space-x-2">
-                  <div className="grid flex-1 gap-2">
-                    <label htmlFor="link" className="sr-only">
-                      Link
-                    </label>
-                    <input
-                      id="link"
-                      defaultValue="https://ui.shadcn.com/docs/installation"
-                      readOnly
-                    />
-                  </div>
-                  <button type="submit" className="px-3">
-                    <span className="sr-only">Copy</span>
-                  </button>
+                <div className="">
+                  <input
+                    required
+                    type="text"
+                    value={delInput}
+                    name="delInput"
+                    placeholder="Enter workspace name"
+                    className="w-full h-[37px] rounded-xl border-[1px] border-gray-200  to-custom-bg-gradient-end pl-3 outline-none text-[15px] text-[#1f1f1f] placeholder-gray-400"
+                    onChange={(e) => setDelInput(e.target.value)}
+                  />
                 </div>
                 <DialogFooter className="sm:justify-start">
                   <DialogClose asChild>
-                    <button type="button">Close</button>
+                    <button
+                      disabled={delInput != currentUser?.organizationName}
+                      type="button"
+                      className="bg-[#E74C3C] text-white py-1 w-full text-[15px] cursor-pointer  font-medium rounded-md "
+                    >
+                      Delete
+                    </button>
                   </DialogClose>
                 </DialogFooter>
               </DialogContent>
