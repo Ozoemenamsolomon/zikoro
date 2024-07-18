b00fcda9606aa9341ed25511f5c7450f8873a0bf
sekani50 <mohammedadeoye0@gmail.com>
2024-07-18 15:16:48 -0700

    feat: added zikoro feedback

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,5 @@
+import AdminFeedBack from "@/components/admin/feedBack/FeedBack";
+
+export default function Page() {
+  return <AdminFeedBack />;
+}
--- src/components/admin/AdminSideBar/AdminSideBar.tsx
+++ src/components/admin/AdminSideBar/AdminSideBar.tsx
@@ -6,6 +6,7 @@ import { adminLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib";
+import { PersonFeedback } from "styled-icons/fluentui-system-filled";
 export function AdminSideBar() {
   const [isNav, setNav] = useState(false);
 
@@ -53,29 +54,51 @@ function SideNav({ close, isNav }: { isNav: boolean; close: () => void }) {
           />
           {/**nav links */}
           <ul className="flex flex-col gap-y-1 items-start  pb-24  no-scrollbar overflow-y-auto  justify-start w-full">
-            {adminLinks.map(({ href, name, image }) => (
-              <li key={name} className="w-full">
-                <Link
-                  href={`/admin/${href}`}
-                 
-                  className={cn(
-                    "p-3 pr-4 pl-4 flex  items-center gap-x-2  w-full",
-                    pathname.includes(href) &&
-                      "text-basePrimary pl-2 pr-4  bg-basePrimary border-l-4 border-basePrimary bg-opacity-10  "
-                  )}
-                >
-                  <Image
-                    alt={name}
-                    src={image}
-                    width={30}
-                    height={30}
-                    className="w-6 h-6"
-                  />
-                  <span>{name}</span>
-                </Link>
-              </li>
-            ))}
+            {adminLinks.map(({ href, name, image }) => {
+              const currentHref = href.includes("?")
+                ? href?.split("?")[0]
+                : href;
+              return (
+                <li key={name} className="w-full">
+                  <Link
+                    href={`/admin/${href}`}
+                    className={cn(
+                      "p-3 pr-4 pl-4 flex  items-center gap-x-2  w-full",
+                      pathname.includes(currentHref) &&
+                        "text-basePrimary pl-2 pr-4  bg-[#001fcc]/10 border-l-4 border-basePrimary bg-opacity-10  "
+                    )}
+                  >
+                    <Image
+                      alt={name}
+                      src={image}
+                      width={30}
+                      height={30}
+                      className="w-6 h-6"
+                    />
+                    <span>{name}</span>
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
+        </div>
+
+        <div className="w-full absolute py-4 bg-[#001fcc]/10 bottom-0 inset-x-0">
+          <div className="w-full bg-white px-3 gap-y-1 flex flex-col items-start justify-start py-8">
+            <p className="w-full text-start">Admin Feedback</p>
+            <p className="text-tiny text-xs w-full">
+              Share this feedback link with users to gather their suggestions
+              and report issues. Help us improve the platform based on user
+              feedback.
+            </p>
+            <Link
+              href="/feedback"
+              className="w-fit px-4 h-10 rounded-md bg-basePrimary text-white flex gap-x-2 items-center justify-center font-medium"
+            >
+              <PersonFeedback size={20} />
+              <p>Feedback</p>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
