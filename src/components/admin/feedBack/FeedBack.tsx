0531a501ac64e6d9914f2e63aecbd98d3c1ff76d
sekani50 <mohammedadeoye0@gmail.com>
2024-07-19 16:06:13 -0700

    fix: uncomment middleware

--- src/components/admin/feedBack/FeedBack.tsx
+++ src/components/admin/feedBack/FeedBack.tsx
@@ -2,7 +2,7 @@ import { FeedBackComp } from "@/components/modals/EventFeedback";
 
 export default function AdminFeedBack() {
   return (
-    <div className="w-full max-w-3xl m-auto min-h-screen flex flex-col items-start justify-center gap-y-4 bg-white py-8 sm:py-10 px-4 sm:px-6">
+    <div className="w-full max-w-2xl m-auto min-h-screen flex flex-col items-start justify-center gap-y-4 bg-white py-8 sm:py-10 px-4 sm:px-6">
       <FeedBackComp />
     </div>
   );
