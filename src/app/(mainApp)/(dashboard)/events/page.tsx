a1bce12607555e1cf4c5481ff7f4ad7822d84473
solozo <ozoemenamsolomon@yahoo.com>
2024-08-31 22:36:58 +0200

    remove unused packages

--- src/app/(mainApp)/(dashboard)/events/page.tsx
+++ src/app/(mainApp)/(dashboard)/events/page.tsx
@@ -1,8 +1,5 @@
 import EventHome from "@/components/eventHome/EventHome";
-// import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default function Page() {
   return <EventHome />;
 }
-
- // export default withPageAuthRequired(Page, { returnTo: "/events" });
