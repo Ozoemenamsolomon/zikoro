b64d72b2c46bf30e115afc1f0e8a2b4edf7c595d
sekani50 <mohammedadeoye0@gmail.com>
2024-09-04 15:32:18 +0100

    fix: issue

--- src/components/payment/paymentModal.tsx
+++ src/components/payment/paymentModal.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 import { ArrowBackOutline } from "styled-icons/evaicons-outline/";
 import React, { useEffect, useState } from "react";
-import { PaymentPlus, PaymentTick } from "@/constants";
+import { PaymentTick } from "@/constants";
 import { PlusCircle, PlusCircleFill } from "styled-icons/bootstrap";
 import { MinusCircle } from "styled-icons/evaicons-solid";
 import { useRouter, usePathname } from "next/navigation";
@@ -68,14 +69,14 @@ type DBOrganisationSchema = {
   facebook: string;
 };
 
-export function PaymentModal({
+export function SubscriptionPaymentModal({
   updateModalState,
   chosenPlan,
   chosenCurrency,
   chosenPrice,
   isChosenMonthly,
 }: Props) {
-  const { user, setUser } = useUserStore();
+  const { user, } = useUserStore();
   const [haveCoupon, setHaveCoupon] = useState<boolean>(false);
   const [isDiscount, setIsDiscount] = useState<boolean>(false);
   const [isRedeemed, setIsRedeemed] = useState<boolean>(false);
