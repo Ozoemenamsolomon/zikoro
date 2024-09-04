d1e04e878f2138d572973399e014e3d7c420595d
emmaudeji-zbook <gogreneinfo@gmail.com>
2024-09-04 20:01:10 +0100

    editted pages

--- src/app/appointments/analytics/page.tsx
+++ src/app/appointments/analytics/page.tsx
@@ -1,38 +1,12 @@
 import React from 'react'
 import Main from '@/components/appointments/Main'
 import Analytics from '@/components/appointments/Analytics'
-// import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-// import { redirect } from 'next/navigation';
-// import { cookies } from "next/headers";
 
 const AnalyticsPage = async () => {
-  // const supabase = createRouteHandlerClient({ cookies });
-
-  // const {
-  //   data: { user },
-  //   error: authError,
-  // } = await supabase.auth.getUser();
-  // if (!user) {
-  //   console.error("Authentication error:", authError?.message);
-  //   redirect('/login')
-  // }
-
-  // const {
-  //   data: userData,
-  //   error: userError,
-  // } = await supabase.from('users').select('*').eq('userEmail', user.email).single();
-
-  // if (!userData) {
-  //   console.error("No user data found for email:", userData.userEmail);
-  //   redirect('/login')
-  // }
-
-  // console.log({user,userData})
-
+   
   return (
     <Main>
       <Analytics />
-      {/* <Analytics user={userData}/> */}
     </Main>
   )
 }
