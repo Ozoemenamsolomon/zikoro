9e71656c2f6b3857a0e6beb848542942e717a83f
sekani50 <mohammedadeoye0@gmail.com>
2024-10-01 07:35:46 -0700

    fwef

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { Toaster } from "react-hot-toast";
 import { TOASTER_PROPS } from "@/lib";
 import { SubscriptionModal } from "../components/contents/_components/modal/SubscriptionModal";
 import { montserrat } from "@/utils/fonts";
-import Head from "next/head"
 
 export const metadata: Metadata = {
   title: "Zikoro",
@@ -20,15 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className=" text-mobile sm:text-desktop">
-    
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-   
-      <body >
+      <body className={`${montserrat.className}`}>
         {children}
         <Toaster {...TOASTER_PROPS} />
         <SubscriptionModal />
