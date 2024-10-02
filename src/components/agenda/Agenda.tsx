57502049acf077a4dd32fd102b2334189a3d48ae
sekani50 <mohammedadeoye0@gmail.com>
2024-10-02 10:57:54 -0700

    fix: updated

--- src/components/agenda/Agenda.tsx
+++ src/components/agenda/Agenda.tsx
@@ -164,7 +164,7 @@ export default function Agenda({
             </div>
           )}
 
-        <div className="w-full p-2 sm:p-4 grid grid-cols-1 items-center gap-8 pb-20">
+        <div className={cn("w-full p-2 sm:p-4 grid grid-cols-1 items-center gap-8 pb-20", isReception && "p-0 sm:p-0")}>
           {fetching && (
             <div className="w-full col-span-full h-[20rem] flex items-center justify-center">
               <LoaderAlt size={30} className="animate-spin" />
