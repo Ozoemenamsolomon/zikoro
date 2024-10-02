a9d4f4c1f6b113eb3492f88af7acc2bff451cf0e
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-10-02 19:40:16 +0100

    rewrite next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-  swcMinify: true,
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-  webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
-    return config;
-  },
 };
 
 module.exports = nextConfig;
