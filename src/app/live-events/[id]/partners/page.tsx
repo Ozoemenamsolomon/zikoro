d39ab289848505772a1c897db23d49c38435b085
sekani50 <mohammedadeoye0@gmail.com>
2024-10-09 08:03:22 -0700

    feat: updated

--- src/app/live-events/[id]/partners/page.tsx
+++ src/app/live-events/[id]/partners/page.tsx
@@ -1,5 +1,5 @@
 import EventPartnerTiers from "@/components/published/EventPartnerTiers";
 
-export default function Page({ params: { id } }: { params: { id: string } }) {
-  return <EventPartnerTiers eventId={id} />;
+export default function Page({searchParams, params: { id } }: {searchParams: any; params: { id: string } }) {
+  return <EventPartnerTiers eventId={id} searchParams={searchParams} />;
 }
