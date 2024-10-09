cb1413c442fbc373cf38f26812bfd430294dfefa
sekani50 <mohammedadeoye0@gmail.com>
2024-10-09 11:04:37 -0700

    fix: issue

--- src/app/(mainApp)/(dashboard)/event/[eventId]/(home)/partners/page.tsx
+++ src/app/(mainApp)/(dashboard)/event/[eventId]/(home)/partners/page.tsx
@@ -4,10 +4,11 @@ import { Partners } from "@/components/partners/Partners";
 interface PageProps {
   params: {
     eventId: string;
-  };
+  }
+
 }
 
-export default async function Page({ params: { eventId } }: PageProps) {
+export default async function Page({  params: { eventId } }: PageProps) {
 
   return <Partners  eventId={eventId} />;
 }
