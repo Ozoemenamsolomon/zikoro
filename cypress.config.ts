326f728faf3969ee9d7f9ae515bfcfc26c0b6df1
solozo <ozoemenamsolomon@yahoo.com>
2024-11-03 18:21:07 +0100

    wip: add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+// import { defineConfig } from "cypress";
+// npm i  -D cypress start-server-and-test
+
+// export default defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {},
+//   },
+//   component: {
+//     devServer: {
+//       framework: "next",
+//       bundler: "webpack",
+//     },
+//   },
+// });
