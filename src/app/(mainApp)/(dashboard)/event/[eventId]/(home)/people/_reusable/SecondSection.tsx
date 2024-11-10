564e91a4c8ff61b757659ce1285d9f469dcdfa11
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-11-10 13:17:45 +0100

    push

--- src/app/(mainApp)/(dashboard)/event/[eventId]/(home)/people/_reusable/SecondSection.tsx
+++ src/app/(mainApp)/(dashboard)/event/[eventId]/(home)/people/_reusable/SecondSection.tsx
@@ -763,7 +763,7 @@ export default function SecondSection({
           )}
         </div>
       </section>
-      {/* {user &&
+      {user &&
         (String(event?.createdBy) === String(user.id) ||
           email === user.userEmail) && (
           <AttendeeCredentials
@@ -774,9 +774,9 @@ export default function SecondSection({
             attendeeCertificatesIsLoading={attendeeCertificatesIsLoading}
             getAttendeeCertificates={getAttendeeCertificates}
           />
-        )} */}
-      {/* <section className="flex justify-between items-center px-2">
-        {!badgeIsLoading &&
+        )}
+      <section className="flex justify-between items-center px-2">
+        {/* {!badgeIsLoading &&
           !attendeeBadge &&
           user &&
           (String(event?.createdBy) === String(user.id) ||
@@ -840,7 +840,7 @@ export default function SecondSection({
                 </span>
               </div>
             </Link>
-          )}
+          )} */}
         {!getEventCertificatesIsLoading &&
           eventCertificates &&
           user &&
@@ -904,7 +904,7 @@ export default function SecondSection({
             <DropdownMenu>
               <DropdownMenuTrigger>
                 <div className=" flex flex-col items-center gap-2 w-fit">
-                  <div className=" w-12 h-12 rounded-[50%] bg-[#F3F3F3] flex items-center justify-center">
+                  <div className="w-12 h-12 rounded-[50%] bg-[#F3F3F3] flex items-center justify-center">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -942,7 +942,7 @@ export default function SecondSection({
               </DropdownMenuContent>
             </DropdownMenu>
           )}
-      </section> */}
+      </section>
       <AttendeeAbout
         city={city}
         country={country}
