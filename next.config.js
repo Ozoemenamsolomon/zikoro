f3b661e193449b71022cbf589a4acc7ad20495c2
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-11-22 11:26:52 +0100

    push

--- next.config.js
+++ next.config.js
@@ -4,8 +4,10 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  
-  
+  webpack(config) {
+    config.externals.push("canvas");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
