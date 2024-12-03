74131c32bf9896ad859625d6adbfbcaf16bfe7c8
Bilal Yusuf <ubahyusuf484@gmail.com>
2024-12-03 21:25:26 +0100

    push

---
+++ src/app/(mainApp)/credentials/create/page.tsx
@@ -0,0 +1,9 @@
+import { Editor } from "@/components/editor/components/editor";
+import React from "react";
+
+const page = () => {
+    const {} = fetchCerti
+  return <Editor />
+};
+
+export default page;
