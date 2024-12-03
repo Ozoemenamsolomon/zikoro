e5540da2d5475f52eab972f768c23e31de4f333d
sekani50 <mohammedadeoye0@gmail.com>
2024-12-03 11:45:41 -0800

    her

--- src/app/live-events/[id]/page.tsx
+++ src/app/live-events/[id]/page.tsx
@@ -3,7 +3,7 @@ import SinglePublishedEvent from "@/components/published/SinglePublishedEvent";
 export default function Page({ params: { id }, searchParams }: {searchParams:any; params: { id: string } }) {
   return (
     <>
-    <head>
+    {/* <head>
     
         <title>
           Testing event thing
@@ -19,7 +19,7 @@ export default function Page({ params: { id }, searchParams }: {searchParams:any
 
         <meta name="author" content="Zikoro" />
   
-    </head>
+    </head> */}
       <SinglePublishedEvent searchParams={searchParams} id={id} />
     </>
   );
