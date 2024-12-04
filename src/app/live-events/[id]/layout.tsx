eb8f516d407f10b38ad5ece0727574acfaed5602
sekani50 <mohammedadeoye0@gmail.com>
2024-12-04 05:24:11 -0800

    erg

--- src/app/live-events/[id]/layout.tsx
+++ src/app/live-events/[id]/layout.tsx
@@ -1,6 +1,34 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 
+export const metaGenerator = async ({
+  params,
+}: {
+  params: { eventId: string };
+}): Promise<Metadata> => {
+  const { eventId } = params;
+  // await fetch(`/api/events/${params.eventId}/event`)
+
+  return {
+    title: `Event Live ${eventId}`,
+    description: "Olaoogergerger",
+
+    // openGraph:{
+    //         type: "website",
+    //         url: "https://example.com",
+    //         title: "My Website",
+    //         description: "My Website Description",
+    //         siteName: "My Website",
+    //         images: [{
+    //           url: "https://example.com/og.png",
+    //         }],
+    //       }
+  };
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-white w-full h-full fixed overflow-y-auto">{children}</div>;
+  return (
+    <div className="bg-white w-full h-full fixed overflow-y-auto">
+      {children}
+    </div>
+  );
 }
--- src/app/live-events/[id]/page.tsx
+++ src/app/live-events/[id]/page.tsx
@@ -1,27 +1,4 @@
 import SinglePublishedEvent from "@/components/published/SinglePublishedEvent";
-import { Metadata } from "next";
-
-export const metaGenerator = async ({ params }: { params: { eventId: string } }): Promise<Metadata> => {
-
-   // await fetch(`/api/events/${params.eventId}/event`)
-
-
-	return {
-    title:"Event Live",
-    description: "Olaoogergerger"
-		
-		// openGraph:{
-    //         type: "website",
-    //         url: "https://example.com",
-    //         title: "My Website",
-    //         description: "My Website Description",
-    //         siteName: "My Website",
-    //         images: [{
-    //           url: "https://example.com/og.png",
-    //         }],
-    //       }
-	};
-};
 
 
 export default function Page({ params: { id }, searchParams }: {searchParams:any; params: { id: string } }) {
