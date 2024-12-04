652fd4da3148683a3c0e3888758c862e34a29f9f
sekani50 <mohammedadeoye0@gmail.com>
2024-12-04 06:42:51 -0800

    wef

--- src/app/live-events/[id]/layout.tsx
+++ src/app/live-events/[id]/layout.tsx
@@ -1,29 +1,4 @@
-import { Metadata } from "next";
 
-export const metaGenerator = async ({
-  params,
-}: {
-  params: { eventId: string };
-}): Promise<Metadata> => {
-  const { eventId } = params;
-  // await fetch(`/api/events/${params.eventId}/event`)
-
-  return {
-    title: `Event Live ${eventId}`,
-    description: "Olaoogergerger",
-
-    // openGraph:{
-    //         type: "website",
-    //         url: "https://example.com",
-    //         title: "My Website",
-    //         description: "My Website Description",
-    //         siteName: "My Website",
-    //         images: [{
-    //           url: "https://example.com/og.png",
-    //         }],
-    //       }
-  };
-};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/live-events/[id]/page.tsx
+++ src/app/live-events/[id]/page.tsx
@@ -1,5 +1,30 @@
 import SinglePublishedEvent from "@/components/published/SinglePublishedEvent";
+import { Metadata } from "next";
 
+export const generateMetadata = async ({
+  params,
+}: {
+params: Promise<{ id: string }>
+}): Promise<Metadata> => {
+  const id = (await params).id
+  // await fetch(`/api/events/${params.eventId}/event`)
+
+  return {
+    title: `Event Live ${id}`,
+    description: "Olaoogergerger",
+
+    // openGraph:{
+    //         type: "website",
+    //         url: "https://example.com",
+    //         title: "My Website",
+    //         description: "My Website Description",
+    //         siteName: "My Website",
+    //         images: [{
+    //           url: "https://example.com/og.png",
+    //         }],
+    //       }
+  };
+};
 
 export default function Page({ params: { id }, searchParams }: {searchParams:any; params: { id: string } }) {
   return (
