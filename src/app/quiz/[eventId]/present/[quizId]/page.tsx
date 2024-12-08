2dd8bc969cca3317fa63ff80b2c7875d3964213d
sekani50 <mohammedadeoye0@gmail.com>
2024-12-08 07:06:43 -0800

    sfsdfs

--- src/app/quiz/[eventId]/present/[quizId]/page.tsx
+++ src/app/quiz/[eventId]/present/[quizId]/page.tsx
@@ -1,4 +1,36 @@
 import Presentation from "@/components/engagements/interactions/_components/presentation/Presentation";
+import { Metadata } from "next";
+
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ quizId: string }>;
+}): Promise<Metadata> => {
+  const quizId = (await params).quizId;
+
+  const response = fetch(`https://zikoro.com/api/quiz/single/${quizId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  const quizDetail = await response;
+
+  return {
+    title: `${quizDetail?.data?.coverTitle || "Zikoro Quiz"} `,
+    description: `${quizDetail?.data?.description ?? ""}`,
+
+    openGraph: {
+      images: [
+     `${quizDetail?.data?.coverImage}` || ""
+      ],
+    },
+  };
+};
+
+
 export default function Page({
   params: { eventId, quizId },
 }: {
