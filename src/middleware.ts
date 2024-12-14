0388e08bd381c785ebf3fb87a36b1bf190781859
sekani50 <mohammedadeoye0@gmail.com>
2024-12-14 12:01:52 -0800

    3f

--- src/middleware.ts
+++ src/middleware.ts
@@ -112,23 +112,23 @@ export async function middleware(req: NextRequest) {
   }
 
  // Check if the request path is included in the protected paths
-  // const isIncludedPath = includedPaths.some((includedPath) =>
-  //   path.startsWith(includedPath)
-  // );
+  const isIncludedPath = includedPaths.some((includedPath) =>
+    path.startsWith(includedPath)
+  );
 
-  // if (isIncludedPath && !session) {
-  //   // If user is not authenticated and path is included, redirect to the login page
-  //   if (path.startsWith("/api")) {
-  //     return NextResponse.json(
-  //       { error: "Authorization failed" },
-  //       { status: 403 }
-  //     );
-  //   } else {
-  //     const redirectUrl = new URL("/login", req.url);
-  //     redirectUrl.searchParams.set("redirectedFrom", path);
-  //     return NextResponse.redirect(redirectUrl);
-  //   }
-  // }
+  if (isIncludedPath && !session) {
+    // If user is not authenticated and path is included, redirect to the login page
+    if (path.startsWith("/api")) {
+      return NextResponse.json(
+        { error: "Authorization failed" },
+        { status: 403 }
+      );
+    } else {
+      const redirectUrl = new URL("/login", req.url);
+      redirectUrl.searchParams.set("redirectedFrom", path);
+      return NextResponse.redirect(redirectUrl);
+    }
+  }
 
   // Allow the request to proceed if the user is authenticated or the path is not included
   return res;
