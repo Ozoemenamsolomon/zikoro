f01b84a233158a14a1bdcac59a296a87d3209980
sekani50 <mohammedadeoye0@gmail.com>
2024-12-15 06:55:20 -0800

    hjhv

--- src/components/SideBarLayout.tsx
+++ src/components/SideBarLayout.tsx
@@ -253,11 +253,10 @@ function SideNavs({
             </div>
             <button
               onClick={() => {
-                localStorage.clear()
+                localStorage.clear();
                 setTimeout(() => {
-                  window.open("/", "_self")
-                },2000)
-               
+                  window.open("/", "_self");
+                }, 2000);
               }}
               className="flex items-center h-fit gap-x-2"
             >
